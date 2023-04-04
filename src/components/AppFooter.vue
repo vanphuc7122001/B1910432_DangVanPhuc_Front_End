6817b73cbc282f4a395d22c706511e0cba8e01fd
B1910432_DangVanPhuc <dangvanphuc7122001@gmail.com>
2023-04-04 15:22:41 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <p>Coypyright by Dang Van Phuc B1910432<br>
+        <a href="mailto:phuc@example.com">Phuc@example.com</a></p>
+    </footer>
+
+</template>
+   
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: DarkSalmon;
+  color: white;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand text-white" >Contacts manager app</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    Contacts
+                    <i class="fas fa-address-book"></i>
+                </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
